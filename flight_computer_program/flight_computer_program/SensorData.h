c4f9ace9710070f0b7d89595432c7f10336d4eb0
Bristine Teneng (bmt756) <bmt756@usask.ca>
2023-10-14 12:34:23 -0600

    Added data types for various sensors as comments to SensorData.h

--- flight_computer_program/flight_computer_program/SensorData.h
+++ flight_computer_program/flight_computer_program/SensorData.h
@@ -17,6 +17,20 @@ public:
     //    - barometer -bmp280 8bit https://www.bosch-sensortec.com/products/environmental-sensors/pressure-sensors/bmp280/#software
     //    - thermocouple - MAX31855 https://learn.adafruit.com/thermocouple/downloads
     //    - fram - adafruit i2c fram
+    /* 
+    
+    bmp280 :   Struct BMP280_data { float Temperature , float  Pressure, float Altitude}
+
+    fram: returns an 8-bit integer when reading a memory address
+
+    MAX31855: float for the temperature 
+
+    kx134: Struct accelerometer_data {float X, float Y, float Z}
+
+    
+    
+    
+    */
 
     //    - flight state
     FlightStates State;
