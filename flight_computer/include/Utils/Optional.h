9b3d95959d9a9f95004c419f9e88fd23f5ec84ea
shadow <tdkuang@gmail.com>
2023-10-22 04:06:30 -0600

    New optional class that can take in optional value

---
+++ flight_computer/include/Utils/Optional.h
@@ -0,0 +1,57 @@
+//
+// Created by TDKua on 2023/10/22.
+//
+
+#ifndef FLIGHT_COMPUTER_OPTIONAL_H
+#define FLIGHT_COMPUTER_OPTIONAL_H
+
+#include <iostream>
+#include <string>
+
+constexpr int32_t NULL_VAL{-128};
+
+template<typename T>
+class Optional
+{
+public:
+	constexpr Optional() = default;
+	constexpr Optional(const T& V) : Value(V), Flag(true){};
+
+	bool HasValue() const { return Flag; }
+	[[nodiscard]] T Get() const { return Flag ? Value : static_cast<T>(NULL_VAL); }
+
+	void operator = (const T& V)
+	{
+		Value = V;
+		Flag = true;
+	}
+
+private:
+	T Value{};
+	bool Flag{false};
+};
+
+template<>
+std::string Optional<std::string>::Get() const
+{
+	return Flag ? Value : "";
+}
+
+template<typename T>
+class Optional<T*>
+{
+public:
+	constexpr Optional() = default;
+	constexpr Optional(T* V) : Value(V){};
+
+	bool HasValue() const { return Value != nullptr; }
+	[[nodiscard]] const T* Get() const { return Value; }
+
+	void operator = (const T& V) { Value = &V; }
+
+	const T operator*() const{ return *Value; }
+
+private:
+	T* Value{nullptr};
+};
+#endif //FLIGHT_COMPUTER_OPTIONAL_H
